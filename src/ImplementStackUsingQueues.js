04c3812fd13a98be8ba4dea20a22aab140b34357
Karim Ellaisy <karim@icomeupmarketing.com>
2017-01-14 17:20:34 -0500

    Create ImplementStackUsingQueues.js

---
+++ src/ImplementStackUsingQueues.js
@@ -0,0 +1,38 @@
+/**
+ * @constructor
+ */
+var Stack = function() {
+    this.stack = []
+};
+
+/**
+ * @param {number} x
+ * @returns {void}
+ */
+Stack.prototype.push = function(x) {
+    this.stack[this.stack.length] = x
+};
+
+/**
+ * @returns {void}
+ */
+Stack.prototype.pop = function() {
+    this.stack.splice(this.stack.length-1, this.stack.length)
+};
+
+/**
+ * @returns {number}
+ */
+Stack.prototype.top = function() {
+    return this.stack[this.stack.length-1]
+};
+
+/**
+ * @returns {boolean}
+ */
+Stack.prototype.empty = function() {
+    if (this.stack.length === 0)
+        return true
+    else
+        return false
+};
